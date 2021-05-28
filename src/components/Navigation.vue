3081bd6e6656d3409b04dd9c9c826eb54e378a7b
Richard <shaodeyu@foxmail.com>
2021-05-28 22:37:17 +0800

    完成了BlogPost组件

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -82,7 +82,8 @@ export default {
 header {
   background-color: #fff;
   padding: 0 25px;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 6px -2px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
   z-index: 99;
   .link {
     font-weight: 500;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="item"
+      v-for="(item, index) in sampleBlogPost"
+      :key="index"
+    />
+  </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost: "Learing the Vuejs in the future",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "Hello world",
+          blogHTML: "Hello world",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "Hello world",
+          blogHTML: "Hello world",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
