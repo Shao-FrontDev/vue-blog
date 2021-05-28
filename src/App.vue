e72dc57f6c9e9617775d0dc0f38f13606ff66273
Richard <shaodeyu@foxmail.com>
2021-05-29 00:02:32 +0800

    完成了BlogCard 组件

--- src/App.vue
+++ src/App.vue
@@ -29,47 +29,5 @@ export default {
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Quicksand", sans-serif;
-}
-
-.app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-.container {
-  max-width: 1440px;
-  margin: 0 auto;
-}
-
-.link {
-  cursor: pointer;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: black;
-}
-
-.link-light {
-  color: #fff;
-}
-
-.arrow {
-  margin-left: 8px;
-  width: 12px;
-  path {
-    fill: #000;
-  }
-}
-
-.arrow-light {
-  path {
-    fill: #fff;
-  }
-}
+@import "./assets/styles/global.scss";
 </style>
